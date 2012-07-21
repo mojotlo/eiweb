0d04e6186deefa5697974f360d45e06e52162611
Taylor Jackson <tsjackson@gmail.com>
2012-07-21 13:54:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eiweb::Application.config.secret_token = '939fef6bcea740de481c54e80b55ba586d838262468583901a332893cf4a5dc71cec78c6035c799180823c8591066cf5a0c63fde69a2b8b3586b1ec25ca79875'
